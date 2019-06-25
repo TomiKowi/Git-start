cd20c2af134a02358c6f6c0bcaab9ca401019a21
Tomek Kowol <tomek.pdp@gmail.com>
2019-06-25 18:55:45 +0200

    isolation maxNumber in FavoriteDogs.js

--- my-app/src/components/FavoriteDogs.js
+++ my-app/src/components/FavoriteDogs.js
@@ -4,17 +4,20 @@ import '../FavoriteDogs.css'
 
 const Favorite = (props) => <div className="imageFavoriteDogsContainer"><img src={props.content} alt="" /><button className="removeButton" onClick={props.remove}>Usuń</button></div>
 
+const maxNumber = 7;
+
+
 const FavoriteDogs = (props) => {
 
-    const { favorites, removeClick } = props
+    const { favorites, removeClick } = props;
 
-    const Favorites = favorites.map(favorite => <Favorite remove={removeClick} content={favorite} />)
+    const Favorites = favorites.map(favorite => <Favorite remove={removeClick} content={favorite} />);
 
-    const numbers = favorites.length
+    const numbers = favorites.length;
 
     return (
         <div className="favoriteContainer">
-            <h3>Twoja kolekcja piesków {`${numbers}/7`}</h3>
+            <h3>Twoja kolekcja piesków {`${numbers}/${maxNumber}`}</h3>
             <div className="favoriteDogsContainer">
                 {Favorites}
             </div>
