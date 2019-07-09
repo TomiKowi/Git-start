97a7d518c5bdfb3e6163486264296edd398c24d1
Tomek Kowol <tomek.pdp@gmail.com>
2019-07-09 20:12:36 +0200

    Updates

---
+++ precache-manifest.79f116c43d6cfbc312af2c10d8126398.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a694b682383d3f130637f84709506d9",
+    "url": "/dogs-API/index.html"
+  },
+  {
+    "revision": "be1f769f4b14bab8fe58",
+    "url": "/dogs-API/static/css/main.be4d532f.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/dogs-API/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "be1f769f4b14bab8fe58",
+    "url": "/dogs-API/static/js/main.b8413008.chunk.js"
+  },
+  {
+    "revision": "85876afc4d9eb8df974e",
+    "url": "/dogs-API/static/js/runtime~main.28322900.js"
+  }
+]);
